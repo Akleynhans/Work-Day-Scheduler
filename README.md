09c6b9b28dcc35212f8fdb77526a8cd52e12ed8a
Kleynhans <Amy.Kleynhans@Emerson.com>
2023-08-01 06:56:01 -0600

    updated code so color of block updated with current time

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work-Day-Scheduler
+
+# find index help https://herewecode.io/blog/replace-item-array-javascript/#:~:text=The%20Definitive%20Guide%22%20%7D-,If%20you%20want%20to%20replace%20an%20object%20in%20an%20array,not%20met%20in%20the%20array.
+
+# for each help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// assign variables
 var savebtn = document.querySelectorAll(".btn");
 var timeBlocksEl = document.querySelectorAll(".time-block");
-// var i = 0;
-var currentHour = dayjs().format('H');
 var array4log = [];
 
 renderText()
@@ -42,6 +38,7 @@ function saveFunction(e) {
   localStorage.setItem("savedText", JSON.stringify(array4log))
 }
 
+// pull saved text from local storage
 function renderText() {
   array4log = JSON.parse(localStorage.getItem("savedText") || "[]");
 
@@ -58,10 +55,9 @@ function clock() {
   // display clock
   $('#currentDay').text(dayjs().format('MMMM D, YYYY h:mm A'));
 
-  // refresh calander at the hour
-  if (dayjs().format('m') == 11) {
-    timeblockloop()
-  }
+  // run function to keep colors current
+  timeblockloop()
+
   // run function on interval to keep updated
   setTimeout(clock, 1000);
 }
@@ -71,17 +67,22 @@ clock()
 
 
 // function to check if timeblock is past present or future and add corresponding class
-
 function colorBlocks(timeblock, i) {
+  // finds the current hour
+  var currentHour = dayjs().format('H');
+  
+  // uses class attributes to assign colors based on the current time
   if (i + 9 == currentHour) {
     timeBlocksEl[i].classList.remove("future");
+    timeBlocksEl[i].classList.remove("past");
     timeBlocksEl[i].classList.add("present");
-
   } else if (i + 9 < currentHour) {
     timeBlocksEl[i].classList.remove("present");
+    timeBlocksEl[i].classList.remove("future");
     timeBlocksEl[i].classList.add("past");
   } else {
     timeBlocksEl[i].classList.remove("past");
+    timeBlocksEl[i].classList.remove("present");
     timeBlocksEl[i].classList.add("future");
   }
 
@@ -91,32 +92,11 @@ function timeblockloop() {
   // run each timeblock through the function
   timeBlocksEl.forEach((timeblock, i) => {
     colorBlocks(timeblock, i)
+   
 
   });
 
 }
-timeblockloop()
 
 
 
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
